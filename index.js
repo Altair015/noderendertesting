e482202626a25b86205b80ccee2c2bd7c81af72c
altair015 <mand23sngh@gmail.com>
2024-08-22 15:21:17 +0530

    test3

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+const jsonwebtoken = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
 const app = express();
+
+console.log(bcrypt)
+console.log(dotenv)
+console.log(jsonwebtoken)
+console.log(mongoose)
 
 app.get("/", (req, res) => {
     res.status(200).send("Node Rendered on Render.");
